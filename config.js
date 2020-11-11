44d3c6ad5cd661b1f89af3bb3b2c5ae2839e0bf2
Dmitriy Voronkov <72332229+VorDmi@users.noreply.github.com>
2020-11-11 23:11:29 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,22 @@
+//Токены
+const vktoken = '';
+const tgtoken = '';
+
+//ID профиля ВК владельца бота
+const owner = 1;
+
+//ID чата телеграмм(присылается при приглашении в беседу)
+const chatId = -1;
+//ID чата вконтакте
+const vk = 2000000001;
+
+//Отправка всех написаных сообщений
+const msgTGtoVK = true;
+const msgVKtoTG = true;
+
+module.exports = {
+    chatId, vk,
+    owner,
+    msgTGtoVK, msgVKtoTG,
+    vktoken, tgtoken
+}
